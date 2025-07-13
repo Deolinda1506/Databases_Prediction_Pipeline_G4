2ae91d95314649595f288db59dc10fcf766dc189
Davdesigner <144008270+Davdesigner@users.noreply.github.com>
2025-07-13 10:05:33 +0200

    linking

--- task2/fastapi_crud_app/database.py
+++ task2/fastapi_crud_app/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Use SQLite for simplicity; change the URI for PostgreSQL/MySQL
-SQLALCHEMY_DATABASE_URL = "sqlite:///./cars.db"
+# Use  PostgreSQL for simplicity and easy deployment
+SQLALCHEMY_DATABASE_URL = "postgresql://codespace@localhost:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
