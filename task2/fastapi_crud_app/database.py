a2cd938bdc38c9889204d9b832fbb9d805c2d624
Gnon Déolinda Bio Bogore <139719649+Deolinda1506@users.noreply.github.com>
2025-07-15 23:24:36 +0200

    Update database.py

--- task2/fastapi_crud_app/database.py
+++ task2/fastapi_crud_app/database.py
@@ -5,6 +5,6 @@ from sqlalchemy.orm import sessionmaker
 # Use  PostgreSQL for simplicity and easy deployment
 SQLALCHEMY_DATABASE_URL = "postgresql://codespace@localhost:5432/postgres"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
